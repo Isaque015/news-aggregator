74ebdba34a5a7003c53eb01014fe6b007e8ea7ed
isaque015 <isaquefer@protonmail.com>
2019-04-02 12:07:07 -0300

    Implementing func for get erros in exceptions

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,12 @@
-from re import match as re_match
+from sqlalchemy import (Column, Integer, String, PrimaryKeyConstraint,
+                        UniqueConstraint)
 
-from sqlalchemy import Column, Integer, String
 from argon2 import PasswordHasher
 
 from app.settings.engine_database import Base, engine
 from app.settings.session_sql import session
+from app.utils.email import tratar_email
+from app.utils.get_errors_exceptions import GetErrorException
 
 
 class User(Base):
@@ -12,56 +14,67 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(80))
-    last_name = Column(String(80))
-    email = Column(String(160))
-    nickname = Column(String(80))
-    passwd = Column(String(160))
+    name = Column(String(80), nullable=False)
+    last_name = Column(String(80), nullable=False)
+    email = Column(String(160), nullable=False, unique=True)
+    nickname = Column(String(80), nullable=False, unique=True)
+    passwd = Column(String(160), nullable=False)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('id', name='pk_user_id'),
+        UniqueConstraint('email', name='unique_email_user'),
+        UniqueConstraint('nickname', name='unique_nickname_user')
+    )
 
     def __repr__(self):
-        return f'User(name="{self.name}", \
-                full_name="{self.name} {self.last_name}",\
-                nickname="{self.nickname}")'
+        return f'{self.nickname}'
 
     def encrypting_password(self):
+        if not self.passwd:
+            return {'status': False, 'msg': 'senha esta vazia'}
+
         password_hash = PasswordHasher()
         self.passwd = password_hash.hash(self.passwd)
-        return
+        return {'status': True, 'msg': 'senha encripitada'}
 
-    def normalize_email(self):
-        result_match_email = re_match(
-            r'^([\w\.\-]+)@([\w]+)(\.)(\w*)((\.(\w*))?)$', self.email
-        )
+    def verificar_senha(self, passwd):
+        ph = PasswordHasher()
+        try:
+            ph.verify(self.passwd, passwd)
+            return {'status': True, 'msg': 'senha correta'}
+        except Exception:
+            return {'status': False, 'msg': 'senha incorreta'}
 
-        if result_match_email:
-            return {'status': True, 'msg': 'Email is valid'}
-
-        if '@' not in self.email:
-            return {'status': False, 'msg': 'not @ in email'}
-
-        nick, domain_name = self.email.split('@')
-
-        if not domain_name:
-            return {'status': False, 'msg': 'not domain name'}
-
-        if '.' not in domain_name:
-            return {'status': False, 'msg': 'not "." in domain name'}
-        return {'status': False, 'msg': 'invalid E-mail'}
+    @tratar_email
+    def normalize_email(self, resposta_email_decorator=None):
+        return resposta_email_decorator
 
     def set_attributes(self, **fields):
         for attribute, value in fields.items():
-            setattr(self, attribute, value)
+            if value:
+                setattr(self, attribute, value)
 
     def save(self):
 
-        self.encrypting_password()
+        result_encrypting_passwd = self.encrypting_password()
+        if not result_encrypting_passwd['status']:
+            return result_encrypting_passwd
 
-        if not self.normalize_email()['status']:
-            return self.normalize_email()['msg']
+        result_normalize_email = self.normalize_email()
+        if not result_normalize_email['status']:
+            return result_normalize_email['msg']
 
-        session.add(self)
-        session.commit()
-        session.close()
+        try:
+            session.add(self)
+            session.commit()
+            return {'status': True, 'msg': 'user save success'}
+        except Exception as e:
+            erro = str(e.__dict__['orig'])
+            get_errors = GetErrorException()
+            msg = get_errors.despacho_func(erro)
+            return {'status': False, 'msg': msg}
+        finally:
+            session.close()
 
 
 Base.metadata.create_all(engine)
